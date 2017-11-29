ea2b26bdbead0559a738b87110b74b1bbee7ba57
Bahaa Eldin <bhaaface@gmail.com>
2017-11-29 03:17:21 +0200

    news api

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,18 @@
 var mainController = angular.module("mainController",[]);
 
-mainController.controller('item1Controller',function($scope)
+mainController.controller('item1Controller',function($scope,$http)
 {
+	$http.get("https://newsapi.org/v2/top-headlines?sources=football-italia&apiKey=f84bcf872efc4197ad7e89c0c86f51bb").then(function(myData){
+		$scope.data = myData.data.articles;
+	});
+	
 	$scope.name = "Hello in item1 Page";
 });
 
-mainController.controller("item2Controller",function($scope)
+mainController.controller("item2Controller",function($scope,$http)
 {
+	$http.get("https://newsapi.org/v2/top-headlines?sources=argaam&apiKey=f84bcf872efc4197ad7e89c0c86f51bb").then(function(myData){
+		$scope.data = myData.data.articles;
+	});
 	$scope.text = "Hello in item2 page";
 });
